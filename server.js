d78db8618df14417a55a4b802ada81c64dc16819
Osvaldo Hernan Ceballos <hernan.casla@gmail.com>
2022-03-23 16:12:26 +0100

    restructuracion

--- server.js
+++ server.js
@@ -1,43 +1,57 @@
-var soap = require('strong-soap').soap;
-var http = require('http');
-var fs = require('fs');
-//send JSON response for stockquote service for GetQuote operation
-var test = {};
-const SERVICE = 'CMM_SERVICES'
+var express = require('express'),
+    app = express(),
+    http = require('http'),
+    server = http.createServer(app),
+    xmlparser = require('express-xml-bodyparser'),
+    path = require('path'),
+    fs = require("fs");
 
-test.server = null;
-test.service = {
-    CMM_SERVICES: {
-        CMM_SERVICES: {
-            processPaymentMethodIntentionsPlay: function (args, cb, soapHeader) {
-                console.log(args)
-                return {processPaymentMethodIntentionsPlayResponse: {generic: {},paymentMethod:{}}};
-            }
-        }
+// .. other middleware ...
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(xmlparser());
+//joining path of directory
+const directoryPath = path.join(__dirname, 'responses');
+var responseFiles = [];
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
     }
-};
-
-//get the WSDL from http://www.webservicex.net/stockquote.asmx?WSDL and save onto current
-//directory as stockquote.wsdl
-//Below code shows how to create a mock up soap server for stockquote wsdl
-fs.readFile('/Users/osvaldoceballos/WebstormProjects/soap-server/processPaymentMethodIntentionsPlay.wsdl', 'utf8', function (err, data) {
-    test.wsdl = data;
-    test.server = http.createServer(function (req, res) {
-        res.statusCode = 404;
-        res.end();
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file);
+        let key = file.replace(/\.[^/.]+$/, "");
+        responseFiles[key] = path.join(directoryPath,file);
     });
+});
+app.post('/', function(req, res, next) {
 
-    test.server.listen(15099, null, function () {
-        test.soapServer = soap.listen(test.server, `/${SERVICE}`, test.service, test.wsdl);
-        test.baseUrl = 'http://' + test.server.address().address + ":" + test.server.address().port;
-        if (test.server.address().address === '0.0.0.0' || test.server.address().address === '::')
-        {
-            test.baseUrl ='http://127.0.0.1:' + test.server.address().port;
-        }
-        //use this URL on the client side to invoke this local web service.
-        //For e.g //http://127.0.0.1:15099/{SERVICE}?wsdl 192.168.1.21
-        console.log(test.baseUrl);
+    // req.body contains the parsed xml
+    console.log(req.body)
 
+    let searchServiceId = (key) => {
+        let result = searchObj(req.body, key);
+        return result[0]?result[0][key][0]:undefined;
+    }
+    let serviceId = searchServiceId('serviceid');
+    console.log(`results: ${serviceId}`)
+    if(responseFiles[serviceId]){
+        fs.readFile(responseFiles[serviceId], 'utf8', function (err, data) {
+            res.send(data);
+        })
+    } else {
+        res.status(404).send('<error>error</error>');
+    }
+});
+function searchObj(obj, key) {
+    if (obj.hasOwnProperty(key)) // or just (key in obj)
+        return [obj];
+    var res = [];
+    Object.entries(obj).forEach(([k, value]) => {
+        if (typeof value == "object" && (value = searchObj(value, key)).length)
+            res.push.apply(res, value);
     });
-})
-
+    return res;
+}
+server.listen(1337);
